a42de08639ea9a253f654d6cf5ae6e1ba0f2d3e3
ChavesSWDev <contactrenchaves@gmail.com>
2025-07-05 12:59:57 -0300

    att

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import usuarioRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.DB_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
