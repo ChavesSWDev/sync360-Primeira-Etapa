72c3df4416f78c14ad36fa95969566db454ae7ea
ChavesSWDev <contactrenchaves@gmail.com>
2025-07-05 12:40:09 -0300

    att server e .env

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,10 @@ import usuarioRoutes from './routes/userRoutes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.APP_PORT || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(cors());
-
 app.use(express.json());
-
 app.use('/usuario', usuarioRoutes);
 
 app.get('/ping', (req, res) => {
